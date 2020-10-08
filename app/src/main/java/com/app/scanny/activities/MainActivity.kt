c12d5382e019e8301b7780543d6b96860fb4cc26
Abhirup Ghosh <joshmcgrath123@gmail.com>
2020-10-09 01:26:17 +0530

    added changes for bindas bol

--- app/src/main/java/com/app/scanny/activities/MainActivity.kt
+++ app/src/main/java/com/app/scanny/activities/MainActivity.kt
@@ -1,42 +1,43 @@
 package com.app.scanny.activities
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.content.pm.PackageManager
-import android.media.MediaRecorder
-import android.media.projection.MediaProjection
-import android.media.projection.MediaProjectionManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.os.IBinder
-import android.provider.Settings
-import android.widget.FrameLayout
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import androidx.core.net.toUri
-import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.app.scanny.BuildConfig
-import com.app.scanny.Constants
+
 import com.app.scanny.R
-import com.app.scanny.databinding.ActivityMainBinding
-import com.app.scanny.fragments.ViewPagerFragment
-import com.app.scanny.service.ScreenCaptureService
-import java.io.File
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_layout)
-        findNavController(R.id.nav_controller)
+        navController = findNavController(R.id.nav_controller)
+
+
+        when(BuildConfig.FLAVOR)
+        {
+            "scanny" -> {
+                navController.graph = navController.navInflater.inflate(R.navigation.nav)
+            }
+
+            "bindasbol" -> {
+                navController.graph = navController.navInflater.inflate(R.navigation.bindas_bol_nav)
+            }
+        }
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
---
+++ app/src/main/java/com/app/scanny/fragments/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package com.app.scanny.fragments
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
