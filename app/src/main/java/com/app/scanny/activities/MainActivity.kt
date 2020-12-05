6c1fd19a96b786ad530e69deca89d13d690814df
Abhirup <Ghosh>
2020-12-05 18:15:36 +0530

    added changes for innterstitial ad

--- app/src/main/java/com/app/scanny/activities/MainActivity.kt
+++ app/src/main/java/com/app/scanny/activities/MainActivity.kt
@@ -12,13 +12,17 @@ import androidx.navigation.ui.NavigationUI
 import com.app.scanny.BuildConfig
 import com.app.scanny.R
 import com.app.scanny.databinding.ActivityHomeLayoutBinding
-import com.app.scanny.repository.Repository
 import com.app.scanny.repository.Repository.remoteConfig
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.firebase.FirebaseApp
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration : AppBarConfiguration
     private lateinit var dataBinding : ActivityHomeLayoutBinding
+    private lateinit var mInterstitialAd : InterstitialAd
+    private var adShown = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,8 +49,9 @@ class MainActivity : AppCompatActivity() {
                 navController.graph = navController.navInflater.inflate(R.navigation.bindas_bol_nav)
             }
 
-            "careercoop"-> {
-                navController.graph = navController.navInflater.inflate(R.navigation.career_coop_nav)
+            "careercoop" -> {
+                navController.graph =
+                    navController.navInflater.inflate(R.navigation.career_coop_nav)
             }
         }
 
@@ -58,32 +65,37 @@ class MainActivity : AppCompatActivity() {
         remoteConfig?.fetch()?.addOnCompleteListener {
 
             if (it.isSuccessful) {
-                Log.d("Remote Config","Success")
+                Log.d("Remote Config", "Success")
                 remoteConfig?.activate()
             }
             else
             {
-                Log.d("Remote Config","Failure")
-                Log.d("Remote Config",it.exception?.message.toString())
+                Log.d("Remote Config", "Failure")
+                Log.d("Remote Config", it.exception?.message.toString())
             }
 
         }
 
     }
 
-    private fun setUpUi()
-    {
+    private fun setUpUi() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
         FirebaseApp.initializeApp(this)
 
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+
+
+
         NavigationUI.setupWithNavController(
             dataBinding.bottomNav,
             navController
         )
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -97,6 +109,19 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            if (mInterstitialAd.isLoaded && adShown.not()) {
+                mInterstitialAd.show()
+                adShown = true
+            }
+        }
+
     }
 
 }
